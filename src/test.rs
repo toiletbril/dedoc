f75555e8654afd16c2022f0238922e74b210109c
toiletbril <s9und@outlook.com>
2023-10-02 19:37:16 +0300

    Add manual testing

--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,18 @@
 #![cfg(debug_assertions)]
 
-use super::*;
-
 use std::fs::{File, remove_file, remove_dir_all};
 use std::io::BufReader;
-use std::sync::Once;
 use std::vec::IntoIter;
 
-use crate::common::{get_program_directory, get_docset_path};
+use crate::common::ResultS;
+use crate::common::get_program_directory;
+use crate::common::{RESET, RED};
 
+use crate::dedoc_debug_println;
+
+use crate::list::list;
 use crate::fetch::fetch;
+use crate::remove::remove;
 use crate::download::download;
 use crate::search::{try_use_cache, search, SearchOptions};
 
@@ -21,61 +24,27 @@ pub fn create_args<'a>(args: &'a str) -> IntoIter<String> {
 }
 
 pub fn remove_program_dir() -> ResultS {
+    dedoc_debug_println!("Removing program directory...");
+
     let program_directory = get_program_directory()?;
+
     remove_dir_all(&program_directory)
         .map_err(|err| format!("Could not remove `{}`: {err}", program_directory.display()))
 }
 
-static SETUP_INIT: Once = Once::new();
+fn run_with_args(command: fn(IntoIter<String>) -> ResultS, args_str: &str, should_do: &str) {
+    let args = create_args(args_str);
+    dedoc_debug_println!("Running with args: `{args_str}`, should {should_do}");
 
-fn setup_test_directory() {
-    SETUP_INIT.call_once(|| {
-        dedoc_debug_println!("Removing program directory...");
-        let _ = remove_program_dir();
-
-        let empty_args = create_args("");
-
-        let fetch_result = fetch(empty_args);
-        assert!(fetch_result.is_ok());
-
-        let download_args = create_args("backbone bower");
-
-        let download_result = download(download_args);
-        assert!(download_result.is_ok());
-
-        let bower_path = get_docset_path(&"bower".to_string())
-            .unwrap();
-        let backbone_path = get_docset_path(&"backbone".to_string())
-            .unwrap();
-
-        assert!(bower_path.exists());
-        assert!(backbone_path.exists());
-    });
-
-    assert!(SETUP_INIT.is_completed());
-
-    dedoc_debug_println!("Directory has been set up.");
-}
-
-fn test_list_local() {
-    let args = create_args("-l");
-
-    dedoc_debug_println!("Running `ls -l`...");
-
-    let list_result = list(args);
-    assert!(list_result.is_ok());
+    let command_result = command(args);
+    assert!(command_result.is_ok());
 }
 
 fn test_search_should_use_cache() {
-    let args = create_args("bower");
-
     let program_directory = get_program_directory().unwrap();
-
     let _ = remove_file(program_directory.join("search_cache_options.json"));
 
-    dedoc_debug_println!("Running `search bower`");
-    let search_result = search(args);
-    assert!(search_result.is_ok());
+    run_with_args(search, "bower", "print search results");
 
     {
         let cache_options_path = program_directory.join("search_cache_options.json");
@@ -90,33 +59,29 @@ fn test_search_should_use_cache() {
     dedoc_debug_println!("Search sucessfully created cache.");
 }
 
-fn test_search_case_insensitive() {
-    let args_str = "backbone -i collection-at";
-    let args = create_args(args_str);
-
-    dedoc_debug_println!("Running `{args_str}`...");
-    let search_result = search(args);
-    assert!(search_result.is_ok());
-
-    let args_str = "backbone -i collection-at -o 1";
-    let args = create_args(args_str);
-
-    dedoc_debug_println!("Running `{args_str}`...");
-    let search_result = search(args);
-    assert!(search_result.is_ok());
-}
-
 // Manual testing. I think this way is better than integration testing I came up with initially.
 // If everything is looking cool, then it's we should be fine :3
 pub fn test<Args>(_args: Args) -> ResultS
 where
     Args: Iterator<Item = String>,
 {
-    setup_test_directory();
+    let _ = remove_program_dir();
 
-    test_list_local();
+    run_with_args(fetch, "", "fetch docs.json");
+    run_with_args(download, "backbone bower", "download docsets");
+
     test_search_should_use_cache();
-    test_search_case_insensitive();
+
+    run_with_args(list, "-l", "list bower and backbone");
+
+    run_with_args(search, "backbone -i collection-at", "list search results in correct casing");
+    run_with_args(search, "backbone -i collection-at -o 1", "open first page");
+    run_with_args(search, "backbone -p map", "list precise search results");
+
+    run_with_args(fetch, "", "show a fetch warning");
+    run_with_args(download, "win", "suggest tailwind");
+    run_with_args(download, "erl", "suggest three erlang versions");
+    run_with_args(remove, "--purge-all", "delete docsets");
 
     dedoc_debug_println!("Tests completed.");
 
