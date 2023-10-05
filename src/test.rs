6ee103a5ff24a368168f163b58dd5f27fc557ae4
toiletbril <s9und@outlook.com>
2023-10-05 08:27:16 +0300

    Add flags for test command

--- src/test.rs
+++ src/test.rs
@@ -7,8 +7,7 @@ use std::vec::IntoIter;
 
 use crate::common::ResultS;
 use crate::common::get_program_directory;
-use crate::common::{RESET, RED};
-
+use crate::common::{RED, GREEN, BOLD, RESET, PROGRAM_NAME};
 use crate::debug_println;
 
 use crate::open::open;
@@ -17,6 +16,23 @@ use crate::download::download;
 use crate::search::{SearchOptions, try_use_cache, search};
 use crate::list::list;
 use crate::fetch::fetch;
+
+use toiletcli::flags::{FlagType, parse_flags};
+use toiletcli::flags;
+
+fn show_test_help() -> ResultS {
+    println!(
+        "\
+{GREEN}USAGE{RESET}
+    {BOLD}{PROGRAM_NAME} test{RESET} [-f] <docset> <page>
+    Run the testing suite.
+
+{GREEN}OPTIONS{RESET}
+    -f, --force                     Run all tests, including `download` and `fetch`.
+        --help                      Display help message."
+    );
+    Ok(())
+}
 
 pub fn create_args<'a>(args: &'a str) -> IntoIter<String> {
     args.split_whitespace()
@@ -35,19 +51,24 @@ pub fn reset_state_and_cache() {
     let _ = remove_file(program_directory.join("search_cache.json"));
 }
 
-fn run_with_args(command: fn(IntoIter<String>) -> ResultS, args_str: &str, should_do: &str) {
+fn run_with_args<O, E>(
+    command: fn(IntoIter<String>) -> Result<O, E>,
+    args_str: &str, should_do: &str
+) -> Result<O,E> {
     let args = create_args(args_str);
     debug_println!("Running with args: `{args_str}`, should {should_do}");
 
     let command_result = command(args);
     assert!(command_result.is_ok());
+
+    command_result
 }
 
-fn test_search_should_use_cache() {
+fn test_search_should_use_cache(args: &str) {
     let program_directory = get_program_directory().unwrap();
     let _ = remove_file(program_directory.join("search_cache_options.json"));
 
-    run_with_args(search, "bower", "print search results");
+    run_with_args(search, args, "print search results");
 
     {
         let cache_options_path = program_directory.join("search_cache_options.json");
@@ -64,13 +85,22 @@ fn test_search_should_use_cache() {
 
 // Manual testing. I think this way is better than integration testing I came up with initially.
 // If everything is looking cool, then it's we should be fine :3
-pub fn test<Args>(args: Args) -> ResultS
+pub fn test<Args>(mut args: Args) -> ResultS
 where
     Args: Iterator<Item = String>,
 {
-    let arg = args.collect::<Vec<String>>().join(" ");
+    let mut flag_help;
+    let mut flag_full;
 
-    if &arg == "full" {
+    let mut flags = flags![
+        flag_help: BoolFlag, ["--help"],
+        flag_full: BoolFlag, ["-f", "--full"]
+    ];
+
+    let args = parse_flags(&mut args, &mut flags)?;
+    if flag_help { return show_test_help(); }
+
+    if flag_full {
         reset_state_and_cache();
 
         run_with_args(fetch, "", "fetch docs.json");
@@ -79,7 +109,7 @@ where
         run_with_args(remove, "backbone bower", "remove backbone and bower if they exist");
         run_with_args(download, "backbone bower", "download docsets");
     } else {
-        debug_println!("Skipping `fetch` and `download`. Run `test full` to avoid skipping, or if you ran `test` first time.");
+        debug_println!("Skipping `fetch` and `download`. Use `-f` flag to avoid skipping.");
     }
 
     run_with_args(download, "win", "suggest tailwind");
@@ -87,11 +117,17 @@ where
 
     run_with_args(list, "-l", "list bower and backbone");
 
-    test_search_should_use_cache();
+    test_search_should_use_cache("backbone");
 
+    run_with_args(search, "backbone -o 1", "open first page");
+    run_with_args(search, "backbone -o 100", "open 100th page");
     run_with_args(search, "backbone -i collection-at", "list search results in correct casing");
     run_with_args(search, "backbone -i collection-at -o 1", "open first page");
     run_with_args(search, "backbone -p map", "list precise search results");
+
+    run_with_args(search, "bower", "should list bower results");
+    run_with_args(search, "bower -o 18", "should open matching result");
+    run_with_args(search, "bower -o 3", "should open matching result");
     run_with_args(search, "bower hahaha nothing", "should result in no matches");
 
     debug_println!("Tests completed.");
