ad74ced1522686e22cb8bbdcd126789cb8a3af75
toiletbril <s9und@outlook.com>
2023-10-06 14:42:59 +0300

    Make tests a little more bearable

--- src/test.rs
+++ src/test.rs
@@ -34,14 +34,14 @@ fn show_test_help() -> ResultS {
     Ok(())
 }
 
-pub fn create_args<'a>(args: &'a str) -> IntoIter<String> {
+fn create_args<'a>(args: &'a str) -> IntoIter<String> {
     args.split_whitespace()
         .map(|s| s.to_string())
         .collect::<Vec<String>>()
         .into_iter()
 }
 
-pub fn reset_state_and_cache() {
+fn reset_state_and_cache() {
     debug_println!("Removing cache...");
 
     let program_directory = get_program_directory().unwrap();
@@ -51,17 +51,21 @@ pub fn reset_state_and_cache() {
     let _ = remove_file(program_directory.join("search_cache.json"));
 }
 
-fn run_with_args<O, E>(
-    command: fn(IntoIter<String>) -> Result<O, E>,
+fn run_with_args<O>(
+    command: fn(IntoIter<String>) -> Result<O, String>,
     args_str: &str, should_do: &str
-) -> Result<O,E> {
+) -> bool {
     let args = create_args(args_str);
     debug_println!("Running with args: `{args_str}`, should {should_do}");
 
     let command_result = command(args);
-    assert!(command_result.is_ok());
 
-    command_result
+    if let Err(err) = &command_result {
+        debug_println!("{BOLD}*** Test failed with: {err} ***");
+        false
+    } else {
+        true
+    }
 }
 
 fn test_search_should_use_cache(args: &str) {
@@ -85,7 +89,7 @@ fn test_search_should_use_cache(args: &str) {
 
 // Manual testing. I think this way is better than integration testing I came up with initially.
 // If everything is looking cool, then it's we should be fine :3
-pub fn test<Args>(mut args: Args) -> ResultS
+pub(crate) fn test<Args>(mut args: Args) -> ResultS
 where
     Args: Iterator<Item = String>,
 {
@@ -112,25 +116,32 @@ where
         debug_println!("Skipping `fetch` and `download`. Use `-f` flag to avoid skipping.");
     }
 
-    run_with_args(download, "win", "suggest tailwind");
     run_with_args(download, "erl", "suggest three erlang versions");
+    run_with_args(download, "win", "suggest tailwind");
 
     run_with_args(list, "-l", "list bower and backbone");
 
     test_search_should_use_cache("backbone");
 
-    run_with_args(search, "backbone -o 1", "open first page");
-    run_with_args(search, "backbone -o 100", "open 100th page");
-    run_with_args(search, "backbone -i collection-at", "list search results in correct casing");
-    run_with_args(search, "backbone -i collection-at -o 1", "open first page");
-    run_with_args(search, "backbone -p map", "list precise search results");
+    debug_println!("Performing search tests...");
 
-    run_with_args(search, "bower", "should list bower results");
-    run_with_args(search, "bower -o 18", "should open matching result");
-    run_with_args(search, "bower -o 3", "should open matching result");
-    run_with_args(search, "bower hahaha nothing", "should result in no matches");
+    let search_results = vec![
+        run_with_args(search, "backbone -o 1", "open first page"),
+        run_with_args(search, "backbone -o 100", "open 100th page"),
+        run_with_args(search, "backbone -i collection-at", "list search results in correct casing"),
+        run_with_args(search, "backbone -i collection-at -o 1", "open first page"),
+        run_with_args(search, "backbone -p map", "list precise search results"),
+        run_with_args(search, "backbone -pi underscore", "list underscore.js with right case"),
+        run_with_args(search, "backbone -o 150", "show model-values"),
 
-    debug_println!("Tests completed.");
+        run_with_args(search, "bower", "list bower results"),
+        run_with_args(search, "bower -o 18", "show update"),
+        run_with_args(search, "bower -o 3", "show cache"),
+        run_with_args(search, "bower hahaha nothing", "should result in no matches"),
+    ];
+
+    let times_search_failed = search_results.iter().filter(|res| !*res).count();
+    debug_println!("Search tests completed, {times_search_failed} tests failed.");
 
     Ok(())
 }
