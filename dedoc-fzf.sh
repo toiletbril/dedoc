ec596bf3ad9c1d8328888cd0944f95857fe6aef1
toiletbril <s9und@outlook.com>
2025-05-08 15:00:28 +0300

    docs: adjust fzf script

--- dedoc-fzf.sh
+++ dedoc-fzf.sh
@@ -14,7 +14,7 @@ fi
 if which moar > /dev/null; then
   PAGER=moar
 elif which less > /dev/null; then
-  PAGER=less
+  PAGER='less -R'
 else
   echo 'ERROR: Please make sure `less` or `moar` is available in $PATH.' >&2
   exit 1
@@ -37,7 +37,7 @@ if test -z "$DEDOC"; then
   exit 1
 fi
 
-if test -z "$1"; then
+if test -z "${1:-}"; then
   echo "USAGE"
   echo "    $0 <docset>"
   echo "    Invoke fzf to interactively search a docset via dedoc."
@@ -48,14 +48,9 @@ DOCSET="$1"
 
 # Make sure the docset we need is installed. Command result substitution below
 # doesn't catch errors, so test it manually.
-T="$($DEDOC ls --porcelain -l -s="$DOCSET")"
-
-if test -z "$T"; then
-  echo "ERROR: \`$DOCSET\` is not downloaded." >&2
-  exit 1
-fi
+"$DEDOC" ls -l --exists="$DOCSET" || exit 1
 
 $DEDOC -c open "$DOCSET" \
 "$("$DEDOC" -c ss "$DOCSET" --porcelain | \
    fzf --ansi --layout=reverse --header-lines=1)" | \
-"$PAGER"
+$PAGER
