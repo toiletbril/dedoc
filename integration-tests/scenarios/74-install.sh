d8a963e651f3692dfe1152b002c265caa0264529
toiletbril <s9und@outlook.com>
2025-11-01 20:57:10 +0300

    add a little test suite for install command

--- integration-tests/scenarios/74-install.sh
+++ integration-tests/scenarios/74-install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-# Install tests.
+# Tests for the install command.
 
 set -eu
 . "$(dirname "$0")"/../scenario-utils.sh
 
-# TODO
+wrapped_dedoc | tail # script warning
+
+wrapped_dedoc install --accept
+head -n 1 < "$DEDOC_HOME"/dedoc-interactive # shebang :3
+
+rm "$DEDOC_HOME"/dedoc-interactive
